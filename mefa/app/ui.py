8db5c8e7a9e91e66461b6dced9087116592ebdd7
git <tindaro.tornabene@gmail.com>
2025-09-12 13:52:06 +0200

    add

--- mefa/app/ui.py
+++ mefa/app/ui.py
@@ -1,22 +1,38 @@
 import streamlit as st
 import requests
+import re
 
 API_URL = "http://localhost:8001"
 
-st.set_page_config(page_title="LLM Function Generator", layout="wide")
-st.title("⚡ LLM Function Generator with LangChain & DSPy")
+st.set_page_config(page_title="VLLM Function Generator", layout="wide")
+st.title("⚡ VLLM Function Generator with LangChain")
 query = st.text_input("Describe the function you want:", "reverse a string")
-
-col1 = st.columns(1)
-
 if st.button("Generate"):
-    with col1:
-        st.subheader("LangChain Result")
-        try:
-            res = requests.get(f"{API_URL}/langchain", params={"query": query}).json()
-            st.code(res["result"], language="python")
-        except Exception as e:
-            st.error(str(e))
+    try:
+        res = requests.get(f"{API_URL}/langchain", params={"query": query}).json()
+        if "result" in res:
+            content= res["result"]["content"].replace("\\n", "\n").replace("\\t", "\t")
+            st.text("1. Try to extract code block if present")
+            code_match = re.search(r"```python(.*?)```", content, re.DOTALL)
+            st.text("code_match = " + str(code_match))
+            if code_match:
+                code = code_match.group(1).strip()
+                st.subheader("📝 Generated Python Function")
+                st.code(code, language="python")
+        with st.expander("🔍 Full response details"):
+                st.json(res)
+
+    except Exception as e:
+        st.error(f"⚠️ API error: {e}")
+
+# if st.button("Generate"):
+#     # with col1:
+#         st.subheader("LangChain Result")
+#         try:
+#             res = requests.get(f"{API_URL}/langchain", params={"query": query}).json()
+#             st.code(res["result"], language="python")
+#         except Exception as e:
+#             st.error(str(e))
 
     # with col2:
     #     st.subheader("DSPy Result")
